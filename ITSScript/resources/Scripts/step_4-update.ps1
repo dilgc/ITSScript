e0facea588bff9e1139e4f5852a318b5e56525bb
dilgc <90067020+dilgc@users.noreply.github.com>
2024-04-30 21:44:33 -0400

    Update step_4-update.ps1
    Made it less dependent on input

--- ITSScript/resources/Scripts/step_4-update.ps1
+++ ITSScript/resources/Scripts/step_4-update.ps1
@@ -1,16 +1,22 @@
-#Update Windows on the machine
-try{
-# Install PSWindowsUpdate module if not already installed
-if (-not (Get-Module -Name PSWindowsUpdate -ListAvailable)) {
-    Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser -AllowClobber
-}
 
-# Import the PSWindowsUpdate module
-Import-Module PSWindowsUpdate -Force
+# Update Windows on the machine
+try {
+    # Install NuGet if not already installed
+    if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+        Install-PackageProvider -Name NuGet -Force -ForceBootstrap
+    }
 
-# Perform Windows Update
-Install-WindowsUpdate -AcceptAll -Verbose
+    # Install PSWindowsUpdate module if not already installed
+    if (-not (Get-Module -Name PSWindowsUpdate -ListAvailable)) {
+        Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser -AllowClobber -Force
+    }
+
+    # Import the PSWindowsUpdate module
+    Import-Module PSWindowsUpdate -Force
+
+    # Perform Windows Update
+    Install-WindowsUpdate -AcceptAll -Verbose
 }
-catch{
-Write-Host "Did not update Windows successfully."
+catch {
+    Write-Host "Did not update Windows successfully."
 }
