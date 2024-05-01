5eb1c594b5cd3b9933e79d69ef0faa0a642ab428
dilgc <90067020+dilgc@users.noreply.github.com>
2024-04-30 20:23:23 -0400

    Update step_0-rename_pc.ps1
    Added workstation description

--- ITSScript/resources/Scripts/step_0-rename_pc.ps1
+++ ITSScript/resources/Scripts/step_0-rename_pc.ps1
@@ -3,15 +3,10 @@ $ITSNumber = Read-Host "Enter ITS Number"
 # Rename PC to "ITS[ITSNumber]"
 Rename-Computer -NewName "ITS$ITSNumber"
 
-Write-Output "PC has been renamed. Will update after a restart."
-Write-Output "Initiating Windows updates..."
+# Prompt user for workstation description
+$WorkstationDescription = Read-Host "Enter Workstation Description"
 
-# Trigger Windows Update via PowerShell
-Invoke-Command -ScriptBlock {
-    $updateSession = New-Object -ComObject Microsoft.Update.Session
-    $updateSearcher = $updateSession.CreateUpdateSearcher()
-    $updates = $updateSearcher.Search("IsInstalled=0")
-    $updates | ForEach-Object {
-        $_.Install()
-    }
-}
+# Set workstation description
+$WMIObject = Get-WmiObject Win32_OperatingSystem
+$WMIObject.Description = $WorkstationDescription
+$WMIObject.Put()
