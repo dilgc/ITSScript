ecc45a2144e7f2b81f0100977d40458bff24c8fe
dilgc <90067020+dilgc@users.noreply.github.com>
2024-04-30 21:24:46 -0400

    Update ITSScript.bat
    Bug fix

--- ITSScript/ITSScript.bat
+++ ITSScript/ITSScript.bat
@@ -26,10 +26,10 @@ if '%errorlevel%' NEQ '0' (
 
 echo Running PowerShell scripts...
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_0-rename_pc.ps1" >> "resources\log.txt"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_1-config_powersettings.ps1" >> "resources\log.txt"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_2-uninstalls.ps1" >> "resources\log.txt"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_3-installs.ps1" >> "resources\log.txt"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_4-update.ps1" >> "resources\log.txt"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_0-rename_pc.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_1-config_powersettings.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_2-uninstalls.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_3-installs.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_4-update.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
 
 pause
