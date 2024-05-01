e36876e77d29bd3ee972e20d5b0f773ac94bf120
dilgc <90067020+dilgc@users.noreply.github.com>
2024-05-01 19:38:31 -0400

    Update step_2-uninstalls.ps1

--- ITSScript/resources/Scripts/step_2-uninstalls.ps1
+++ ITSScript/resources/Scripts/step_2-uninstalls.ps1
@@ -1,2 +1,2 @@
-#Coming soon
+#This is difficult, because McAfee resists silent uninstalls since it is a security product, and Office has so many versions.
 Write-Output "Uninstallation automation is a WIP. Please uninstall manually."
