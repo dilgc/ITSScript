d74e8da70ccf8975d145a822e2828547c126b8b8
dilgc <90067020+dilgc@users.noreply.github.com>
2024-04-30 20:40:28 -0400

    Update step_0-rename_pc.ps1
    Added error handling

--- ITSScript/resources/Scripts/step_0-rename_pc.ps1
+++ ITSScript/resources/Scripts/step_0-rename_pc.ps1
@@ -1,12 +1,35 @@
-# Prompt user for ITS number
-$ITSNumber = Read-Host "Enter ITS Number"
-# Rename PC to "ITS[ITSNumber]"
-Rename-Computer -NewName "ITS$ITSNumber"
+# Prompt user for ITS number. Rename PC to ITS[Number]
+
+try {
+
+    $ITSNumber = Read-Host "Enter ITS Number"
+    Rename-Computer -NewName "ITS$ITSNumber" -ErrorAction Stop
+    
+    Write-Host "PC has been renamed to $ITSNumber successfully."
+}
+catch
+{
+    $errorMessage = $_.Exception.Message
+    Write-Host "Error occurred trying to rename PC"
+    Write-Host "Error: $errorMessage"
+}
+
 
 # Prompt user for workstation description
-$WorkstationDescription = Read-Host "Enter Workstation Description"
 
-# Set workstation description
-$WMIObject = Get-WmiObject Win32_OperatingSystem
-$WMIObject.Description = $WorkstationDescription
-$WMIObject.Put()
+try{
+    $WorkstationDescription = Read-Host "Enter Workstation Description"
+
+    # Set workstation description
+    $WMIObject = Get-WmiObject Win32_OperatingSystem
+    $WMIObject.Description = $WorkstationDescription
+    $WMIObject.Put() -ErrorAction Stop
+
+    Write-Host "Description has been set to $WorkstationDescription successfully."
+}
+catch
+{
+    $errorMessage = $_.Exception.Message
+    Write-Host "Error occurred trying to set workstation description"
+    Write-Host "Error: $errorMessage"
+}
