75f381425aebd09ad04e94bab7493505c733e97f
dilgc <90067020+dilgc@users.noreply.github.com>
2024-04-30 21:38:31 -0400

    Update ITSScript.bat
    bug fixes

--- ITSScript/ITSScript.bat
+++ ITSScript/ITSScript.bat
@@ -26,10 +26,10 @@ if '%errorlevel%' NEQ '0' (
 
 echo Running PowerShell scripts...
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_0-rename_pc.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_1-config_powersettings.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_2-uninstalls.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_3-installs.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_4-update.ps1" 2>&1 | Tee-Object -FilePath "resources\log.txt" -Append
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_0-rename_pc.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_1-config_powersettings.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_2-uninstalls.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_3-installs.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_4-update.ps1"
 
 pause
