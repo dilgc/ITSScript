3c304e2cae8233b39edd760ce7136d6ae0fb9c92
dilgc <90067020+dilgc@users.noreply.github.com>
2024-04-30 20:56:50 -0400

    Update ITSScript.bat
    Create a log.txt

--- ITSScript/ITSScript.bat
+++ ITSScript/ITSScript.bat
@@ -26,10 +26,10 @@ if '%errorlevel%' NEQ '0' (
 
 echo Running PowerShell scripts...
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_0-rename_pc.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_1-config_powersettings.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_2-uninstalls.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_3-installs.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_4-update.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_0-rename_pc.ps1" >> "resources\log.txt"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_1-config_powersettings.ps1" >> "resources\log.txt"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_2-uninstalls.ps1" >> "resources\log.txt"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_3-installs.ps1" >> "resources\log.txt"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "resources\Scripts\step_4-update.ps1" >> "resources\log.txt"
 
 pause
